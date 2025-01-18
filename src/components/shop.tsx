48b3d22b4b708d1524e669981e939e502955879b
8bitbug <akrammohadin@gmail.com>
2025-01-18 21:57:38 +0100

    Cleaning up

--- src/components/shop.tsx
+++ src/components/shop.tsx
@@ -1,6 +1,7 @@
 import { createSignal, For } from "solid-js";
 import { game, setGame } from "./game";
 import Autoclicker from "../assets/shop/autoclicker.png";
+import Chicken from "../assets/shop/chicken.png";
 import Border from "../assets/decoration/wood.jpg";
 
 const originalWarn = console.warn;
@@ -13,7 +14,6 @@ console.warn = function(message, ...optionalParams) {
   originalWarn.apply(console, [message, ...optionalParams]);
 };
 
-
 export interface ProducerBaseProps {
   name: string,
   img: string,
@@ -22,16 +22,7 @@ export interface ProducerBaseProps {
   amount: number,
 }
 
-interface ProducerBaseOptions extends ProducerBaseProps {
-  save?: boolean,
-}
-
-const ProducerBase = (options: ProducerBaseOptions) => {
-  const { save, ...props } = options;
-  if (save) {
-    game.producers.push(props);
-  }
-
+const ProducerBase = (props: ProducerBaseProps) => {
   const [cost, setCost] = createSignal(props.cost);
   const [amount, setAmount] = createSignal(props.amount);
 
@@ -41,10 +32,13 @@ const ProducerBase = (options: ProducerBaseOptions) => {
       setGame("producers", producers => {
         const index = producers.findIndex(p => p.name === props.name);
         if (index !== -1) {
-          producers[index].cost *= 1.2;
-          producers[index].amount++;
+          producers[index] = {
+            ...producers[index],
+            cost: producers[index].cost * 1.2,
+            amount: producers[index].amount + 1
+          };
         }
-        return producers;
+        return [...producers];
       });
       setCost(props.cost * 1.2);
       setAmount(props.amount + 1);
@@ -88,8 +82,28 @@ const ProducerBase = (options: ProducerBaseOptions) => {
   );
 }
 
-
 export const Shop = () => {
+  const requiredProducers = [
+    { name: "Autoclicker", img: Autoclicker, eps: 0.1, cost: 25, amount: 0 },
+    { name: "Chicken", img: Chicken, eps: 1, cost: 100, amount: 0 }
+  ];
+
+  setGame("producers", producers => {
+    producers = producers.filter(p => requiredProducers.some(rp => rp.name === p.name));
+
+    requiredProducers.forEach(producer => {
+      if (!producers.some(p => p.name === producer.name)) {
+        producers.push(producer);
+      }
+    });
+
+    producers.sort((a, b) => {
+      return requiredProducers.findIndex(rp => rp.name === a.name) - requiredProducers.findIndex(rp => rp.name === b.name);
+    });
+
+    return producers;
+  });
+
   if (game.producers.length >= 1) {
     return (
       <div style={{
@@ -103,23 +117,8 @@ export const Shop = () => {
         "background-color": "rgba(0,0,0,0.222)",
       }}>
         <For each={game.producers} fallback={<div>Loading...</div>}>
-          {(item) => <ProducerBase name={item.name} img={item.img} eps={item.eps} cost={item.cost} amount={item.amount} save={true} />}
+          {(item) => <ProducerBase name={item.name} img={item.img} eps={item.eps} cost={item.cost} amount={item.amount} />}
         </For>
-      </div>
-    )
-  } else {
-    return (
-      <div style={{
-        position: "absolute",
-        right: "0",
-        top: "0",
-        width: "20rem",
-        height: "100vh",
-        "border-image": `url(${Border}) 50 round`,
-        "border-left": "10px solid",
-        "background-color": "rgba(0,0,0,0.222)",
-      }}>
-        <ProducerBase name="Autoclicker" img={Autoclicker} eps={0.1} cost={10} amount={0} save={true} />
       </div>
     )
   }
